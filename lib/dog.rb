0e55dfe521a253af0e9d7dffcaf3dc59e5becf26
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-21 20:03:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
 class Dog
-  def initialize(name)
-  @name= name
-end
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
 
-def name=(name)
-  @name=(name)
-end
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
 
-def name
-  @name
-end
-end
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
+end 
